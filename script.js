414ceefb15a23ed1b2911f5c4813b9f8f475e779
Decknova <97154335+Decknova@users.noreply.github.com>
2026-01-27 13:34:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-fetch("data.json?v=10")
+fetch("data.json?v=99")
 .then(r => r.json())
 .then(data => {
-  const list = document.getElementById("list");
+  const q = new URLSearchParams(location.search);
+  const user = q.get("user");
+  if (!user) return;
 
-  Object.keys(data).forEach(k => {
-    if (k !== "tugas") {
-      const card = document.createElement("div");
-      card.className = "user-card";
-      card.innerText = data[k].nama;
-      card.onclick = () => {
-        location.href = `detail.html?user=${k}`;
+  document.getElementById("nama").innerText = data[user].nama;
+  const tbody = document.querySelector("#tabel tbody");
+  const days = ["Ahad","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+
+  data.tugas.forEach(tugas => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${tugas}</td>`;
+
+    days.forEach(day => {
+      const key = `${user}-${tugas}-${day}`;
+      const status = localStorage.getItem(key);
+
+      const td = document.createElement("td");
+      td.className = "cell" + (status === "done" ? " done" : "");
+      td.innerText = status === "done" ? "✓" : "";
+
+      td.onclick = () => {
+        if (td.classList.contains("done")) {
+          td.classList.remove("done");
+          td.innerText = "";
+          localStorage.removeItem(key);
+        } else {
+          td.classList.add("done");
+          td.innerText = "✓";
+          localStorage.setItem(key, "done");
+        }
       };
-      list.appendChild(card);
-    }
+
+      tr.appendChild(td);
+    });
+
+    tbody.appendChild(tr);
   });
 });
